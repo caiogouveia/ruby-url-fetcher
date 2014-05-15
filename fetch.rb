f427ff9f1280b221128ce6753db824a75ec8c9f2
Caio Gouveia <caiomdm@gmail.com>
2014-05-15 19:37:17 -0300

    lots of things

--- fetch.rb
+++ fetch.rb
@@ -2,6 +2,14 @@
 require 'nokogiri'
 require 'open-uri'
 require 'openssl'
+require 'ruby-progressbar'
+
+def colorize(text, color_code)
+  "\e[#{color_code}m#{text}\e[0m"
+end
+
+def red(text); colorize(text, 31); end
+def green(text); colorize(text, 32); end
 
 
 abort "Exemple of usage: $#{$0} http://awesomewebsite.com outputFile.txt" if (ARGV.size != 2)
@@ -11,35 +19,51 @@ abort "Exemple of usage: $#{$0} http://awesomewebsite.com outputFile.txt" if (AR
 
 # Get a Nokogiri::HTML:Document for the page we’re interested in...
 puts "Fetching web page ..."
+
 @doc = Nokogiri::HTML(open(@url,{ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}))
-puts "Fetched."
+if @doc.nil?
+  abort red('Something went wrong ...')
+else
+  puts  green('Fetched.')
+end
 
-# Do funky things with it using Nokogiri::XML::Node methods...
+puts "Extracting hyperlinks..."
 
-# puts doc.css('html')
-####
-# Search for nodes by css
-# doc.css('#res #search #ires #rso div li div h3.r a').each do |link|
-#   puts link.content
-# end
-#
-# puts doc.xpath('//a[@href]').map {
-#   |link| [link.text.strip, link["href"]]
-# }
-
-puts "Extracting hiperlinks..."
-# hLinksHash = {}
-# hLinksHash[count] = tempUrl
-tmpHyperlinks = ''
+#Varibles
+tempArray = Array.new
 count = 1
+progress = ProgressBar.create(:starting_at => 0, :total => 200)
+
 @doc.xpath('//a[@href]').each do |link|
-  tempUrl = @url+link['href']
-  tmpHyperlinks  += tempUrl+"\n"
+  #se for do mesmo dominio colocar no array
+  if !link['href'].to_s.include? "http://"
+    if !link['href'].to_s.include? "https://"
+    tempUrl = @url+"/"+link['href']
+    tempArray << tempUrl+"\n"
+    end
+  end
+
+  if count <= 200
+    progress.increment
+  end
+
   count+=1
 end
-# puts hLinksHash
+
+#make strings uniq in the array
+tempArray.uniq!
 
 # Write to file
-File.open(@output, 'w') { |file|
-  file.write(tmpHyperlinks)
-}
+# File.open(@output, 'w') { |file|
+#   file.write(tempArray.join(""))
+# }
+
+#use webkit2png to take screenshots of the pages
+# progress = ProgressBar.create(:starting_at => 0, :total => 200)
+
+
+tempArray.each do |item|
+  puts "Fetching page: "+item
+  exec "webkit2png -W 1366 -F "+item
+  puts green('Done...')
+end
